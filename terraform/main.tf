6ede75f668ccd0adf22bd8cbcf3ff4a010d7f3d6
kazeem <aniyalorungade@gmail.com>
2023-10-09 11:54:57 -0400

    new change to code

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,11 +14,8 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-# locals {
-#   cluster_name = "utrains-eks-${random_string.suffix.result}"
-# }
+locals {
+  cluster_name = "myapp-eks-cluster"
+}
 
-# resource "random_string" "suffix" {
-#   length  = 6
-#   special = false
-# }
+
