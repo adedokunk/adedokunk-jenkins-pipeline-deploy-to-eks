bafa70d59903acad80e68ac0dd229b21103a814c
kazeem <aniyalorungade@gmail.com>
2023-10-09 11:09:04 -0400

    new change to terraform code

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -30,7 +30,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
+      version = "~> 5.20.0"
     }
 
     random = {
@@ -50,7 +50,7 @@ module "eks" {
   version = "18.26.6"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.23"
+  cluster_version = "1.24"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
