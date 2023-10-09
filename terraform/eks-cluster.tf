ddef440906fe5a52b70d598c0098fd515a3d3d0e
kazeem <aniyalorungade@gmail.com>
2023-10-09 11:24:36 -0400

    new change to code

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,56 +1,9 @@
-# module "eks" {
-#     source  = "terraform-aws-modules/eks/aws"
-#     version = "~> 19.0"
-#     cluster_name = "myapp-eks-cluster"
-#     cluster_version = "1.27"
-    
-
-#     cluster_endpoint_public_access  = true
-
-#     vpc_id = module.myapp-vpc.vpc_id
-#     subnet_ids = module.myapp-vpc.private_subnets
-
-#     tags = {
-#         environment = "development"
-#         application = "myapp"
-#     }
-
-#     eks_managed_node_groups = {
-#         dev = {
-#             min_size = 1
-#             max_size = 3
-#             desired_size = 2
-
-#             instance_types = ["t2.small"]
-#         }
-#     }
-# }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.20.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-
-
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.26.6"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.24"
+  cluster_version = "1.23"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -102,4 +55,3 @@ module "eks" {
     }
   }
 }
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
-# variable "vpc_cidr_block" {}
-# variable "private_subnet_cidr_blocks" {}
-# variable "public_subnet_cidr_blocks" {}
-
-# variable "enable_classiclink" {
-#   type    = bool
-#   default = null
-# }
-
 variable "region" {
   description = "AWS region"
   type        = string
